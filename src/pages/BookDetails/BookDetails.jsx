66cabf1abb1633e1a76709c84c79694e3cd67962
roscxn <roscxn@gmail.com>
2023-03-31 17:02:57 +0800

    Show Author Name

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -24,19 +24,16 @@ const BookDetails = () => {
         {book.category} <p/>
         {book.genre}, {book.language}, {book.isbn} <p/>
         {book.publishDate} by {book.publisher}<p/>
-    
+        {book?.author?.name}    
       <p/>
-        {/* <h1>BookDetails</h1> <br/>
-        <h2>(Insert Picture)</h2>
-        <h2>Book Name</h2>
-        <h3>Author:</h3>
+
         <h3>Copies Available:</h3>
         <Link to={`/books/${id}/borrow`}>
         <button>Borrow</button>
         </Link>
         <Link to={`/books/${id}/return`}>
         <button>Return</button>
-        </Link> */}
+        </Link>
         </>
     )
 }
